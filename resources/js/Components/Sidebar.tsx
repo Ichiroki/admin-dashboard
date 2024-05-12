114d7c8f62a711a4e0c541e8413c028e312c512b
Ichiroki <fahrezirizqiawan12649@gmail.com>
2024-05-12 08:59:01 +0700

    Dashboard: add navigation

--- resources/js/Components/Sidebar.tsx
+++ resources/js/Components/Sidebar.tsx
@@ -4,17 +4,13 @@ import ApplicationLogo from './ApplicationLogo';
 import NavLink from './NavLink';
 import route from 'ziggy-js';
 import { Icon } from './Icons';
+import clsx from 'clsx';
 
 function Sidebar() {
     return (
         <>
-            <nav className='flex justify-center'>
-            <ul className='w-9/12 flex gap-3 flex-col mt-6'>
-                <li>
-                    <Link href={route('dashboard')}>
-                        <ApplicationLogo className="block h-9 w-auto" />
-                    </Link>
-                </li>
+            <nav className={clsx('flex justify-center text-lg bg-violet-400 sidebar sidebar h-screen')}>
+            <ul className='w-9/12 flex gap-3 flex-col'>
                 <li className='mt-6'>
                     <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                         <Icon color='white' iconName="HouseDoor" className='w-6 h-6 mr-4'/>
--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -48,13 +48,87 @@ export default function AppLayout({
   }
 
   return (
-    <div>
+    <div className='layout'>
       <Head title={title} />
 
-      <Banner />
+      {/* <Banner /> */}
 
-      <div className='min-h-screen w-3/12 bg-violet-400 text-lg'>
-        <Sidebar />
+      <div className='min-h-screen w-[19%]'>
+        <div className="navigation">
+            <nav className='flex justify-between items-center bg-violet-400 w-screen px-8 py-5'>
+                <Link href={route('dashboard')}>
+                    <ApplicationLogo className="block h-9 w-auto" />
+                </Link>
+                <Dropdown
+                    align="right"
+                    width="48"
+                    renderTrigger={() =>
+                      page.props.jetstream.managesProfilePhotos ? (
+                        <button className="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                          <img
+                            className="h-8 w-8 rounded-full object-cover"
+                            src={page.props.auth.user?.profile_photo_url}
+                            alt={page.props.auth.user?.name}
+                          />
+                        </button>
+                      ) : (
+                        <span className="inline-flex rounded-md">
+                          <button
+                            type="button"
+                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150"
+                          >
+                            {page.props.auth.user?.name}
+
+                            <svg
+                              className="ml-2 -mr-0.5 h-4 w-4"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                              />
+                            </svg>
+                          </button>
+                        </span>
+                      )
+                    }
+                  >
+                    {/* <!-- Account Management --> */}
+                    <div className="block px-4 py-2 text-xs text-gray-400">
+                      Manage Account
+                    </div>
+
+                    <DropdownLink href={route('profile.show')}>
+                      Profile
+                    </DropdownLink>
+
+                    {page.props.jetstream.hasApiFeatures ? (
+                      <DropdownLink href={route('api-tokens.index')}>
+                        API Tokens
+                      </DropdownLink>
+                    ) : null}
+
+                    <div className="border-t border-gray-200 dark:border-gray-600"></div>
+
+                    {/* <!-- Authentication --> */}
+                    <form onSubmit={logout}>
+                      <DropdownLink as="button">Log Out</DropdownLink>
+                    </form>
+                  </Dropdown>
+            </nav>
+        </div>
+        <div>
+            <Sidebar />
+        </div>
+        <div className='main'>
+            test
+        </div>
+        <div className='footer'>
+            test
+        </div>
       </div>
     </div>
   );
